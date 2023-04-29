fbfbef5e7b83ecdaba297a5ba4f4946396590224
MoonFuji <mohammed.kadri@ensia.edu.dz>
2023-04-30 00:35:15 +0200

    added the dashboard page to add object to database

--- db.php
+++ db.php
@@ -5,14 +5,11 @@ $dbuser = "root";
 $dbpass = "";
 $dbname = "4_arms";
 // Create database connection
-mysqli_connect($host, $dbuser, $dbpass, $dbname);
+$conn = new mysqli($host, $dbuser, $dbpass, $dbname);
 // Check connection
-if(mysqli_connect_error())
-{
-echo "Connection establishing failed! <br >";
-}
-else
-{
-echo "Connection established successfully. <br >";
+if (mysqli_connect_error()) {
+    echo "Connection establishing failed! <br >";
+} else {
+    echo "Connection established successfully. <br >";
 }
 ?>;
