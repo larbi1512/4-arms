6edbce8965d1eee9fc72c039e4bf81005128e9ce
MoonFuji <mohammed.kadri@ensia.edu.dz>
2023-05-15 19:45:17 +0200

    edit & delete functional, new page 'see all'

--- 4-arms.sql
+++ 4-arms.sql
@@ -1,5 +1,5 @@
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -13,41 +13,59 @@ USE `4-arms`;
 
 CREATE TABLE `activity` (
   `user_id` int(11) NOT NULL,
-  `time_activity` int(11) DEFAULT NULL,
-  `month` varchar(4) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `month` varchar(4) DEFAULT NULL,
+  `activity_time` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `activity` (`user_id`, `time_activity`, `month`) VALUES
-(1, 12, 'Jan'),
-(1, 18, 'Feb'),
-(1, 56, 'mar'),
-(1, 17, 'apr'),
-(1, 12, 'may');
+INSERT INTO `activity` (`user_id`, `month`, `activity_time`) VALUES
+(1, '5', 0.0001875),
+(1, '5', 0.000269167),
+(1, '5', 0.000169722),
+(1, '5', 0.000150833),
+(1, '5', 0.000508333),
+(1, '5', 0.000233611),
+(1, '5', 0.000242222),
+(1, '5', 0.000248333),
+(1, '5', 0.000174444),
+(1, '5', 0.000342778),
+(1, '5', 0.000324444),
+(1, '5', 0.000620833),
+(1, '5', 0.000464444),
+(1, '5', 0.00225944),
+(1, '5', 0.00255556),
+(1, '5', 0.000301111),
+(1, '5', 0.000555556),
+(1, '5', 0.000299167),
+(1, '5', 0.0003975),
+(1, '5', 0.000804444);
 
 CREATE TABLE `assigned` (
   `user_id` int(11) NOT NULL,
   `workout_id` int(11) NOT NULL,
   `diet_id` int(11) NOT NULL,
   `gym_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `assigned` (`user_id`, `workout_id`, `diet_id`, `gym_id`) VALUES
-(1, 2, 2, NULL),
-(1, 4, 4, NULL);
-
-CREATE TABLE `chat` (
-  `message_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+(1, 2, 2, 1),
+(1, 4, 4, 1);
 
 CREATE TABLE `diet` (
   `diet_id` int(11) NOT NULL,
   `diet_link` varchar(100) DEFAULT NULL,
   `diet_name` varchar(50) DEFAULT NULL,
   `diet_link_img` varchar(100) DEFAULT NULL,
-  `diet_calories` int(11) DEFAULT NULL,
-  `diet_protien` int(11) DEFAULT NULL,
-  `diet_carbs` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `calories` int(11) DEFAULT NULL,
+  `proteins` int(11) DEFAULT NULL,
+  `carbs` int(11) DEFAULT NULL,
+  `description` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `diet` (`diet_id`, `diet_link`, `diet_name`, `diet_link_img`, `calories`, `proteins`, `carbs`, `description`) VALUES
+(1, './diet/diet01/diet01.pdf', 'Green blender', './diet/diet01/diet1.jpg', 300, 150, 100, 'Nourish your body with nutrient-dense foods, prioritizing fruits, vegetables, lean proteins, whole grains, and healthy fats to revitalize your energy and restore nutritional balance.'),
+(2, './diet/diet01/diet01.pdf', 'Be Extra', './diet/diet01/diet1.jpg', 200, 150, 210, 'Optimize your metabolism and support muscle development with a strategic blend of high-quality proteins, complex carbohydrates, and essential fats, fueling your body for increased energy and a transformative physique.'),
+(3, './diet/diet03/diet03.jpg', 'Do Not Feel Stressita, Feel Bonita', './diet/diet03/diet3.jpg', 500, 300, 200, 'Practice mindful eating and make sustainable food choices, prioritizing whole foods and plant-based options to nourish your body while considering the environmental impact of your dietary decisions for long-term well-being.'),
+(4, './diet/diet04/diet04.pdf', 'SparklingSoul Regimen', './diet/diet04/diet4.jpg', 250, 150, 100, 'Support your bodys natural detoxification processes by focusing on detoxifying foods and incorporating cleansing techniques to eliminate toxins, promoting internal rejuvenation and renewed vitality.');
 
 CREATE TABLE `gym` (
   `gym_id` int(11) NOT NULL,
@@ -57,7 +75,7 @@ CREATE TABLE `gym` (
   `city` varchar(50) DEFAULT NULL,
   `gym_img_link` varchar(300) DEFAULT NULL,
   `gym_link` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `gym` (`gym_id`, `gym_name`, `gym_address`, `gym_number`, `city`, `gym_img_link`, `gym_link`) VALUES
 (1, 'Extreme Gym', '101 Nasr Al Den st. - From Haram st. - Giza, After Masjed Nasr Al Den', 792771935, 'Oran', './Gym/ExtremeGym/ExtremeGym.png', 'https://goo.gl/maps/Vbh31da7ynQcPU8e7'),
@@ -70,24 +88,34 @@ CREATE TABLE `message` (
   `date_sending` date NOT NULL,
   `destination_id` int(11) DEFAULT NULL,
   `seen` bit(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `orders` (
   `product_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `date_order` date NOT NULL,
+  `date_order` date NOT NULL DEFAULT current_timestamp(),
   `date_delivery` date DEFAULT NULL,
   `user_address` varchar(300) DEFAULT NULL,
-  `quantity` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `quantity` int(11) DEFAULT NULL,
+  `phone_number` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `orders` (`product_id`, `user_id`, `date_order`, `date_delivery`, `user_address`, `quantity`, `phone_number`, `name`) VALUES
+(1, 1, '2023-05-10', '2023-05-25', 'bir el djir oran', 1, 792765351, 'hafida boukedjar'),
+(1, 1, '2023-05-10', '2023-05-25', 'bir el djir oran', 1, 792765351, 'hafida boukedjar'),
+(1, 1, '2023-05-10', '2023-05-25', 'bir el djir oran', 1, 792765351, 'hafida boukedjar');
 
 CREATE TABLE `product` (
   `price` float NOT NULL,
-  `product_ID` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
   `product_name` varchar(50) NOT NULL,
   `product_category` varchar(50) NOT NULL,
   `png_link` varchar(90) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `product` (`price`, `product_id`, `product_name`, `product_category`, `png_link`) VALUES
+(1300, 1, 'Whey Protein', '', './public/whey.png');
 
 CREATE TABLE `progress` (
   `calories` float DEFAULT NULL,
@@ -96,46 +124,38 @@ CREATE TABLE `progress` (
   `wined_weight` float DEFAULT NULL,
   `lost_weight` float DEFAULT NULL,
   `user_id` int(11) NOT NULL,
-  `last_update` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `last_update` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `progress` (`calories`, `protien`, `carbs`, `wined_weight`, `lost_weight`, `user_id`, `last_update`) VALUES
 (12, 12, 12, 12, 12, 1, '2015-12-17');
 
 CREATE TABLE `team` (
   `member_id` int(11) NOT NULL,
-  `memebr_name` varchar(70) DEFAULT NULL,
-  `memebr_email` varchar(300) DEFAULT NULL,
-  `member_number` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `member_name` varchar(70) DEFAULT NULL,
+  `member_email` varchar(300) DEFAULT NULL,
+  `member_number` int(11) DEFAULT NULL,
+  `memeber_img_link` varchar(500) DEFAULT NULL,
+  `memeber_link` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `team` (`member_id`, `memebr_name`, `memebr_email`, `member_number`) VALUES
-(1, 'Dayaa Bou', 'BouDaia@gmail.com', 775171633),
-(2, 'Baraa Lalagui', 'Baraa@gmail.com', 771298765),
-(3, 'Hichem BKR', 'hichembkr@gmail.com', 555181966);
-
-CREATE TABLE `user_info` (
-  `user_ID` int(11) NOT NULL,
-  `weight` float DEFAULT NULL,
-  `hight` float DEFAULT NULL,
-  `goal` float DEFAULT NULL,
-  `calories` float DEFAULT NULL,
-  `protien` float DEFAULT NULL,
-  `carbs` float DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `team` (`member_id`, `member_name`, `member_email`, `member_number`, `memeber_img_link`, `memeber_link`) VALUES
+(1, 'Dayaa Bou', 'BouDaia@gmail.com', 775171633, NULL, NULL),
+(2, 'Baraa Lalagui', 'Baraa@gmail.com', 771298765, NULL, NULL),
+(3, 'Hichem BKR', 'hichembkr@gmail.com', 555181966, NULL, NULL);
 
 CREATE TABLE `user_signup` (
   `user_name` varchar(30) NOT NULL,
   `user_email` varchar(255) NOT NULL,
   `user_ID` int(11) NOT NULL,
-  `user_password` int(11) NOT NULL,
-  `age` int(11) DEFAULT NULL,
-  `gender` varchar(1) DEFAULT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `weight` int(4) DEFAULT NULL,
+  `height` int(4) DEFAULT NULL,
   `user_img` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `user_signup` (`user_name`, `user_email`, `user_ID`, `user_password`, `age`, `gender`, `user_img`) VALUES
-('hafida', 'bou@gmail.com', 1, 12, 12, 'f', './avatar.jpg');
+INSERT INTO `user_signup` (`user_name`, `user_email`, `user_ID`, `user_password`, `weight`, `height`, `user_img`) VALUES
+('hafida', 'bou@gmail.com', 1, '121212', NULL, NULL, './avatar.jpg');
 
 CREATE TABLE `workout_plan` (
   `workout_id` int(11) NOT NULL,
@@ -145,26 +165,25 @@ CREATE TABLE `workout_plan` (
   `workout_plan_link` varchar(300) DEFAULT NULL,
   `description` varchar(500) DEFAULT NULL,
   `workout_summary` varchar(500) DEFAULT NULL,
-  `Goal` varchar(500) DEFAULT NULL,
+  `goal` varchar(500) DEFAULT NULL,
   `duration` int(11) DEFAULT NULL,
-  `lenght` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `length` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `workout_plan` (`workout_id`, `workout_planer_id`, `workout_name`, `workout_img_link`, `workout_plan_link`, `description`, `workout_summary`, `Goal`, `duration`, `lenght`) VALUES
-(1, 1, 'BuildMuscle', './workout_plans/BuildMuscle/BuildMuscle.png', './workout_plans/BuildMuscle/BuildMuscle.pdf', 'For a better fit building a mass of muscles\r\n', 'The workout plan is a 3-6 day split you can follow for the next 6 weeks to build muscle. Its focus is to help increase muscle gain and strength development. The muscle building program is suitable for beginners and intermediates.\r\n\r\nYour rep tempo should be slow and controlled. Focus on the eccentric contraction of the muscle. For example when you are lowering the barbell during a curl, a bench press, or when you’re lowering yourself on a chest dip.', 'Build Muscle', 6, 0),
+INSERT INTO `workout_plan` (`workout_id`, `workout_planer_id`, `workout_name`, `workout_img_link`, `workout_plan_link`, `description`, `workout_summary`, `goal`, `duration`, `length`) VALUES
 (2, 2, 'GitFit', './workout_plans/GetFit/GetFit.png', './workout_plans/GetFit/GetFit.pdf', 'This program can help you establish a fitness mindset that can then lead to better body composition, less-fat-more-muscle. Unless you get it together in the kitchen, don’t expect to lose fat. In fact, if your nutrition is really bad, you may continue to gain fat.', 'Splits are a popular way to organize exercises into workouts which are performed on specified days. Those can be specific calendar days (Monday, Wednesday, Friday) or simply Day 1, Day 2 and so forth. The latter is helpful for people whose work or school schedules vary week to week.\r\n\r\nUpper-Lower splits divide the body at the waist, generally speaking. Chest, shoulders, back, arms, and core are “Upper”, and legs are “Lower” with the exception that some leg exercises involve the back and arms.\r\n', 'Build And Tone', 8, 0),
 (3, 1, 'DefineYou', './workout_plans/DefineYou/DefineYou.png', './workout_plans/DefineYou/DefineYou.pdf\r\n ', 'It is a program that ensures the define of the body in 8 weeks making use of the inner burn of the fat', 'Gaining muscle should be priority, no matter how much fat you lose, you need to put on some lean muscle if you want to improve the way you look and feel. The less muscle you have means the lower your body fat percentage has to be to see it. Gaining muscle also helps you to burn more calories; a pound of fat burns about 2-3 calories per day while a pound of muscle can burn 7- 10 calories daily. Aim for at least three workout sessions per week and make sure to perform the bigger compound lifts suc', 'Define Your body and Muscles', 8, NULL),
-(4, 3, 'Fat Desctroyer', './workout_plans/FatDestroyer/FatDestroyer.png', './workout_plans/FatDestroyer/fatdestroyer.pdf', 'To lose weight in a healthy non toxic way for a better fit and a good progress', 'Fat Burner\r\nWhey Protein\r\nBCAA Intra-Workout\r\nFish Oil (EFAs)\r\nCreatine Monohydrate', 'Lose Fat', 12, NULL);
+(4, 3, 'Fat Desctroyer', './workout_plans/FatDestroyer/FatDestroyer.png', './workout_plans/FatDestroyer/fatdestroyer.pdf', 'To lose weight in a healthy non toxic way for a better fit and a good progress', 'Fat BurnerWhey ProteinBCAA Intra-WorkoutFish Oil (EFAs)Creatine Monohydrate', 'Lose Fat', 12, 5);
 
+
+ALTER TABLE `activity`
+  ADD KEY `fk_activity` (`user_id`);
 
 ALTER TABLE `assigned`
   ADD PRIMARY KEY (`user_id`,`workout_id`,`diet_id`),
   ADD KEY `fk_workout_id` (`workout_id`),
   ADD KEY `fk_diet_id` (`diet_id`),
   ADD KEY `fk_gym_id` (`gym_id`);
-
-ALTER TABLE `chat`
-  ADD PRIMARY KEY (`message_id`);
 
 ALTER TABLE `diet`
   ADD PRIMARY KEY (`diet_id`);
@@ -177,11 +196,11 @@ ALTER TABLE `message`
   ADD KEY `fk_destination` (`destination_id`);
 
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`product_id`,`user_id`,`date_order`),
-  ADD KEY `fk_user` (`user_id`);
+  ADD KEY `fk_user` (`user_id`),
+  ADD KEY `fk_product` (`product_id`);
 
 ALTER TABLE `product`
-  ADD PRIMARY KEY (`product_ID`);
+  ADD PRIMARY KEY (`product_id`);
 
 ALTER TABLE `progress`
   ADD PRIMARY KEY (`user_id`,`last_update`);
@@ -189,41 +208,47 @@ ALTER TABLE `progress`
 ALTER TABLE `team`
   ADD PRIMARY KEY (`member_id`);
 
-ALTER TABLE `user_info`
-  ADD KEY `f_user_info` (`user_ID`);
-
 ALTER TABLE `user_signup`
   ADD PRIMARY KEY (`user_ID`);
 
 ALTER TABLE `workout_plan`
-  ADD PRIMARY KEY (`workout_id`);
+  ADD PRIMARY KEY (`workout_id`),
+  ADD KEY `fk_workout_planer_id` (`workout_planer_id`);
 
-
-ALTER TABLE `diet`
-  MODIFY `diet_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
-ALTER TABLE `gym`
-  MODIFY `gym_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 ALTER TABLE `product`
-  MODIFY `product_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 ALTER TABLE `user_signup`
-  MODIFY `user_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 ALTER TABLE `workout_plan`
-  MODIFY `workout_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `workout_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
+
+ALTER TABLE `activity`
+  ADD CONSTRAINT `fk_activity` FOREIGN KEY (`user_id`) REFERENCES `user_signup` (`user_ID`);
+
+ALTER TABLE `assigned`
+  ADD CONSTRAINT `fk_diet_id` FOREIGN KEY (`diet_id`) REFERENCES `diet` (`diet_id`),
+  ADD CONSTRAINT `fk_gym_id` FOREIGN KEY (`gym_id`) REFERENCES `gym` (`gym_id`),
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user_signup` (`user_ID`),
+  ADD CONSTRAINT `fk_workout_id` FOREIGN KEY (`workout_id`) REFERENCES `workout_plan` (`workout_id`);
 
 ALTER TABLE `message`
   ADD CONSTRAINT `fk_destination` FOREIGN KEY (`destination_id`) REFERENCES `team` (`member_id`),
   ADD CONSTRAINT `fk_sender` FOREIGN KEY (`sender_id`) REFERENCES `user_signup` (`user_ID`);
 
+ALTER TABLE `orders`
+  ADD CONSTRAINT `fk_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_ID`),
+  ADD CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `user_signup` (`user_ID`);
+
 ALTER TABLE `progress`
   ADD CONSTRAINT `fk_progress` FOREIGN KEY (`user_id`) REFERENCES `user_signup` (`user_ID`);
 
-ALTER TABLE `user_info`
-  ADD CONSTRAINT `f_user_info` FOREIGN KEY (`user_ID`) REFERENCES `user_signup` (`user_ID`);
+ALTER TABLE `workout_plan`
+  ADD CONSTRAINT `fk_workout_planer_id` FOREIGN KEY (`workout_planer_id`) REFERENCES `team` (`member_id`);
+SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- admin/insert.php
+++ admin/insert.php
@@ -1,35 +1,32 @@
 <?php
 require('../db.php');
-$table = $_GET['table'];
-$field_names = array();
-
-switch ($table) {
-    case "gym":
-        $table_name = "gym";
-        $field_names = array("gym_name", "gym_address", "gym_number");
-        break;
-    case "diet":
-        $table_name = "diet";
-        $field_names = array("diet_link", "diet_name", "carbs", "calories", "proteins");
-        break;
-    case "workout":
-        $table_name = "workout_plan";
-        $field_names = array("workout_name", "workout_img_link", "workout_plan_link");
-        break;
-    case "product":
-        $table_name = "product";
-        $field_names = array("price", "product_id", "product_name", "product_category", "png_link");
-        break;
-    default:
-        return false;
+if (isset($_GET['action']) && $_GET['action'] == 'delete') {
+    deleterecord($_GET['table']);
+    exit();
 }
 
+$table = $_GET['table'];
+$field_names = array();
+$table_map = array(
+    "gym" => array("gym_name", "gym_address", "city", "gym_number", "gym_img_link", "gym_link"),
+    "diet" => array("diet_link", "diet_name", "diet_link_img", "carbs", "calories", "proteins"),
+    "workout" => array("workout_name", "workout_img_link", "workout_plan_link", "description", "workout_summary", "goal", "duration", "length"),
+    "product" => array("price", "product_name", "product_category", "png_link")
+);
+if (isset($table_map[$table])) {
+    if ($table == 'workout') {
+        $table_name = 'workout_plan';
+    } else {
+        $table_name = $table;
+    }
+    $field_names = $table_map[$table];
+} else {
+    die("Invalid table name");
+}
 function insertrecord($table_name, $field_names)
 {
     global $conn;
-
     // Construct the SQL query and execute it
-
     foreach ($field_names as $key => $value) {
         if (isset($_POST[$value])) {
             $field_values[] = "'" . mysqli_real_escape_string($conn, $_POST[$value]) . "'";
@@ -45,44 +42,99 @@ function insertrecord($table_name, $field_names)
     return true;
 }
 
+function updaterecord($table_name, $field_names)
+{
+    global $conn;
 
+    // Construct the SQL query and execute it
 
-
-if (isset($_GET['table']) && ($_GET['table'] == 'gym' || $_GET['table'] == 'diet' || $_GET['table'] == 'workout' || $_GET['table'] == 'product')) {
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        insertrecord($_GET['table'], $field_names);
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
+    foreach ($field_names as $key => $value) {
+        if (isset($_POST[$value])) {
+            $field_values[] = $value . "='" . mysqli_real_escape_string($conn, $_POST[$value]) . "'";
+        }
     }
-} else {
-    die("Invalid table name.");
+
+    // Construct the SQL query and execute it
+    if ($table_name == 'workout_plan') {
+        $update_query = "UPDATE " . $table_name . " SET " . implode(", ", $field_values) . " WHERE workout_id=" . $id;
+    } else {
+
+        $update_query = "UPDATE " . $table_name . " SET " . implode(", ", $field_values) . " WHERE " . $table_name . "_id=" . $id;
+    }
+    mysqli_query($conn, $update_query) or die(mysqli_error($conn));
+    return true;
 }
 
-?>
 
+function deleterecord($table_name)
+{
+    global $conn;
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
+    if ($table_name == 'workout') {
+        $delete_query = "DELETE FROM workout_plan WHERE workout_id=" . $id;
+    } else {
+        $delete_query = "DELETE FROM " . $table_name . " WHERE " . $table_name . "_id=" . $id;
+    }
+    mysqli_query($conn, $delete_query) or die(mysqli_error($conn));
+    header("Location: dashboard.php?success=Record deleted successfully");
+    return true;
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
+
+    $get_query = "SELECT * FROM " . $table_name . " WHERE " . $table . "_id=$id";
+    $result = mysqli_query($conn, $get_query) or die(mysqli_error($conn));
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        foreach ($field_names as $key => $value) {
+            $_POST[$value] = $row[$value];
+        }
+    }
+}
+
+
+
+// Output the HTML form with the appropriate action URL and button label
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="utf-8">
-    <title>Insert New <?php echo ucfirst($_GET['table']); ?></title>
-    <link rel="stylesheet" href="css/style.css" />
+    <title><?php echo isset($id) ? 'Update' : 'Insert New'; ?> <?php echo ucfirst($table); ?></title>
+    <link rel="stylesheet" href="insert.css" />
 </head>
 
 <body>
     <div class="form">
         <p><a href="dashboard.php">Dashboard</a>
-            | <a href="view.php">View Records</a>
-            | <a href="logout.php">Logout</a></p>
+            | <a href="dashboard.php">View Records</a>
         <div>
-            <h1>Insert New <?php echo ucfirst($_GET['table']); ?></h1>
+            <h1><?php echo isset($id) ? 'Update' : 'Insert New'; ?> <?php echo ucfirst($table); ?></h1>
             <form name="form" method="post" action="">
-                <input type="hidden" name="new" value="1" />
                 <?php foreach ($field_names as $key => $value) { ?>
-                    <p><input type="text" name="<?php echo $value; ?>" placeholder="Enter <?php echo ucfirst($key); ?>" required /></p>
+                    <p><input type="text" name="<?php echo $value; ?>" placeholder="Enter <?php echo ucfirst($value); ?>" value="<?php echo isset($_POST[$value]) ? htmlspecialchars($_POST[$value]) : ''; ?>" required /></p>
                 <?php } ?>
-                <p><input name="submit" type="submit" value="Submit" /></p>
+                <p><input name="submit" type="submit" value="<?php echo isset($id) ? 'Update' : 'Insert'; ?>" /></p>
             </form>
         </div>
     </div>
 </body>
 
 </html>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_POST['submit'] == 'Insert') {
+        if (insertrecord($table_name, $field_names)) {
+            header("Location: dashboard.php?success=Record inserted successfully");
+        }
+    } else if ($_POST['submit'] == 'Update') {
+        if (updaterecord($table_name, $field_names)) {
+            header("Location: dashboard.php?success=Record updated successfully");
+        }
+    }
+}
+?>
