8429915bd87cf2e8f0ca725f05a26534c4252ef5
MoonFuji <mohammed.kadri@ensia.edu.dz>
2023-05-15 19:54:17 +0200

    fixes around all the files, still a long way to go

--- Quiz/insert_data.php
+++ Quiz/insert_data.php
@@ -1,22 +1,11 @@
 <?php
 session_start();
+require_once "../db.php";
 $choice = $_GET["choice"];
-$user_id=$_SESSION["user_id"];
+$user_id = $_SESSION["user_id"];
 // Replace the placeholders with actual values
 $sql = "INSERT INTO assigned (user_id, workout_id, diet_id, gym_id) VALUES (1, '$choice', '$choice', NULL)";
 
-// Connect to the database
-$host = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "4-arms";
-// Create database connection
-$conn = new mysqli($host, $dbuser, $dbpass, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
 
 // Execute the query
 if ($conn->query($sql) === TRUE) {
@@ -29,4 +18,3 @@ if ($conn->query($sql) === TRUE) {
 
 // Close the connection
 $conn->close();
-?>
