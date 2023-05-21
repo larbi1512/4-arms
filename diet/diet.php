07c0edc391d03415fd356aad085a7448c269020b
larbi <99117566+larbi1512@users.noreply.github.com>
2023-05-21 18:49:15 +0100

    added the profile + the pop-up button to diet after a long fight with that shit

--- diet/diet.php
+++ diet/diet.php
@@ -44,7 +44,7 @@ if (isset($_POST['carbs_consumed']) && isset($_POST['calories_consumed']) && iss
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="initial-scale=1, width=device-width" />
-  <link rel="stylesheet" href="./global.css" />
+  <link rel="stylesheet" href="global.css" />
   <link rel="stylesheet" href="diet.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nasalization:wght@400&display=swap" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" />
@@ -76,42 +76,91 @@ if (isset($_POST['carbs_consumed']) && isset($_POST['calories_consumed']) && iss
 
     <div class="nav-bar sticky-nav">
       <div class="nav-bar1">
-        <a class="home" id="home" href="../newHome.html">Home</a>
+        <a class="home" id="home" href="../NewHome/newHome.php">Home</a>
         <a class="workout" id="workout" href="../workout/workout.php">Workout</a>
         <a class="diet1" href="./diet.php">Diet</a><a class="supplement" href="../Shop/supplement.html">Supplement</a>
       </div>
-    </div>
-
-
-    <button class="login-button" id="profile">
+          <button class="login-button" id="profile">
       <div class="profile1">Profile</div>
     </button>
+     </div>
 
+   
 
-    <div id="pROFILEContainer" class="popup-overlay" style="display: none">
-      <div class="profile">
-        <div class="profile-child"></div>
-        <img class="profile-item" alt="" src="./public/ellipse-800.svg" />
-        <div class="ls">LS</div>
-        <div class="larbi-saidchikh">Larbi SAIDCHIKH</div>
-        <div class="larbisckgmailcom">larbisck@gmail.com</div>
-        <div class="my-progress">my progress</div>
-        <div class="my-diet">my diet</div>
-        <div class="settings">Settings</div>
-        <div class="log-out">Log out</div>
-        <div class="my-supplements">my supplements</div>
+      <div id="pROFILEContainer" class="popup-overlay" style="display: none">
+        <div class="profile">
+          <?php
+          $query = $conn->query(
+            "select * from user_signup where user_id=$user_id"
+          );
+          foreach ($query as $data) {
+            $user_name = $data['user_name'];
+            $user_email = $data['user_email'];
+            $user_img = $data['user_img'];
+          }
+          ?>
+          <div class="profile-child">
+            <img class="profile-item" alt="" src="./public/ellipse-800.svg" />
+            <img src="<?php echo $user_img ?>"
+              style="height:25%;width:17%; border-radius:15%;margin-right:20%;margin-left:10%;margin-top:4%" />
+          </div>
+          <div class="larbi-saidchikh" style="margin-top:6%">
+            <?php echo $user_name ?>
+          </div>
+          <div class="larbisckgmailcom" style="margin-top:2%">
+            <?php echo $user_email ?>
+          </div>
+          <div class="my-progress">
+            <a href="../workout/workout.php" style="color:black !important">My progress</a>
+          </div>
+          <div class="my-diet">
+            <a href="./diet.php" style="color:black !important">My diet</a>
+          </div>
+          <div class="log-out">
+            <a href="../signup_login/login.php">Log out</a>
+          </div>
+          <div class="my-supplements">
+            <a href="../Shop/supplement.php" style="color:black !important">My supplements</a>
+          </div>
+        </div>
       </div>
-    </div>
+        <script>
+      var loginButton = document.getElementById("profile");
+      if (loginButton) {
+        loginButton.addEventListener("click", function() {
+          var popup = document.getElementById("pROFILEContainer");
+          if (!popup) return;
+          var popupStyle = popup.style;
+          if (popupStyle) {
+            popupStyle.display = "flex";
+            popupStyle.zIndex = 100;
+            popupStyle.backgroundColor = "rgba(113, 113, 113, 0.3)";
+            popupStyle.alignItems = "center";
+            popupStyle.justifyContent = "center";
+          }
+          popup.setAttribute("closable", "");
 
-    <h1 class=" top">Your daily nutrition:</h1>
+          var onClick =
+            popup.onClick ||
+            function(e) {
+              if (e.target === popup && popup.hasAttribute("closable")) {
+                popupStyle.display = "none";
+              }
+            };
+          popup.addEventListener("click", onClick);
+        });
+      }
+    </script>
+
+       
+
+    
+        <h1 class=" top">Your daily nutrition:</h1>
+
     <p class="text">Eat healthy while having fun :3 </p>
 
-
-
-    <!-- Add the remaining values to the HTML table -->
-    <!-- Display the form with remaining values -->
-    <div class="scrollable-container">
     <form method="post" action="" class="form-container">
+
       <table>
         <thead>
           <tr>
@@ -150,25 +199,27 @@ if (isset($_POST['carbs_consumed']) && isset($_POST['calories_consumed']) && iss
       </table>
       <input type="submit" value="Check Progress">
     </form>
-  </div>
-
-    <div class="recommended-activities">Your diet </div>
+        <div class="recommended-activities">Your diet </div>
     <div class="plan">
       <div class="plan-img">
         <a href="<?php echo $row['diet_link'] ?>">
-          <img src="<?php echo $row['diet_link_img'] ?>" alt="" onmouseover="this.style.opacity=1" onmouseout="this.style.opacity=0.7">
-        </a>
-      </div>
-      <div class="plan-description">
-        <p style="font-size:36px">
-          <?php echo $row['diet_name'] ?>
-        </p>
-        <p style="font-size:70%">
-          <?php echo $row['description'] ?>
-        </p>
-        <a href="<?php echo $row['diet_link'] ?>">Learn More</a>
-      </div>
-    </div>
+              <img src="<?php echo $row['diet_link_img'] ?>" alt="" onmouseover="this.style.opacity=1"
+                onmouseout="this.style.opacity=0.7">
+            </a>
+          </div>
+          <div class="plan-description">
+            <p style="font-size:36px">
+              <?php echo $row['diet_name'] ?>
+            </p>
+            <p style="font-size:70%">
+              <?php echo $row['description'] ?>
+            </p>
+            <a href="<?php echo $row['diet_link'] ?>">Learn More</a>
+          </div>
+        </div>
+     </div>
+
+
     <style>
       .recommended-activities {
         margin-top: 28%;
@@ -185,7 +236,7 @@ if (isset($_POST['carbs_consumed']) && isset($_POST['calories_consumed']) && iss
         width: 95%;
         padding: 20px;
         margin-top: 3%;
-        z-index: 9999;
+        z-index: 99;
       }
 
       .plan-img {
@@ -222,83 +273,68 @@ if (isset($_POST['carbs_consumed']) && isset($_POST['calories_consumed']) && iss
 
       }
     </style>
-  </div>
   <script>
-    var dietText = document.getElementById("dietText");
-    if (dietText) {
-      dietText.addEventListener("click", function(e) {
-        window.location.href = "./diet1.html";
-      });
-    }
-
-    var loginButton = document.getElementById("profile");
-    if (loginButton) {
-      loginButton.addEventListener("click", function() {
-        var popup = document.getElementById("pROFILEContainer");
-        if (!popup) return;
-        var popupStyle = popup.style;
-        if (popupStyle) {
-          popupStyle.display = "flex";
-          popupStyle.zIndex = 100;
-          popupStyle.backgroundColor = "rgba(113, 113, 113, 0.3)";
-          popupStyle.alignItems = "center";
-          popupStyle.justifyContent = "center";
-        }
-        popup.setAttribute("closable", "");
-
-        var onClick =
-          popup.onClick ||
-          function(e) {
-            if (e.target === popup && popup.hasAttribute("closable")) {
-              popupStyle.display = "none";
-            }
-          };
-        popup.addEventListener("click", onClick);
-      });
-    }
-
-    var home = document.getElementById("home");
-    if (home) {
-      home.addEventListener("click", function(e) {
-        // Please sync "4-arms" to the project
-      });
-    }
-
-    var workout = document.getElementById("workout");
-    if (workout) {
-      workout.addEventListener("click", function(e) {
-        // Please sync "Workout " to the project
-      });
-    }
-
-    document.querySelector('form').addEventListener('submit', function() {
-      var carbs_consumed = parseInt(document.querySelector('input[name="carbs_consumed"]').value);
-      var calories_consumed = parseInt(document.querySelector('input[name="calories_consumed"]').value);
-      var proteins_consumed = parseInt(document.querySelector('input[name="proteins_consumed"]').value);
-      var carbs_consumed += carbs_consumed;
-      var calories_consumed += calories_consumed;
-      var proteins_consumed += proteins_consumed;
-      var carbs_remaining = 100 - carbs_consumed;
-      var calories_remaining = 2000 - calories_consumed;
-      var proteins_remaining = 200 - proteins_consumed;
+   document.querySelector('form').addEventListener('submit', function() {
+    var carbs_consumed = parseInt(document.querySelector('input[name="carbs_consumed"]').value);
+    var calories_consumed = parseInt(document.querySelector('input[name="calories_consumed"]').value);
+    var proteins_consumed = parseInt(document.querySelector('input[name="proteins_consumed"]').value);
+    carbs_consumed += carbs_consumed;
+    calories_consumed += calories_consumed;
+    proteins_consumed += proteins_consumed;
+    var carbs_remaining = <?php echo $row['carbs'] ?> - carbs_consumed;
+    var calories_remaining = <?php echo $row['Calories'] ?> - calories_consumed;
+    var proteins_remaining = <?php echo $row['Proteins'] ?> - proteins_consumed;
 
       document.querySelector('span.remaining:nth-of-type(1)').textContent = carbs_remaining;
-      document.querySelector('span.remaining:nth-of-type(2)').textContent = calories_remaining;
-      document.querySelector('span.remaining:nth-of-type(3)').textContent = proteins_remaining;
-    });
+    document.querySelector('span.remaining:nth-of-type(2)').textContent = calories_remaining;
+    document.querySelector('span.remaining:nth-of-type(3)').textContent = proteins_remaining;
 
-    window.addEventListener("scroll", function() {
-  var navbar = document.querySelector(".diet-child");
-  var formContainer = document.querySelector(".form-container");
-  if (window.pageYOffset > navbar.offsetTop) {
-    navbar.classList.add("sticky-nav");
-    formContainer.style.marginTop = navbar.offsetHeight + "px";
-  } else {
-    navbar.classList.remove("sticky-nav");
-    formContainer.style.marginTop = "0";
-  }
 });
   </script>
+
+    <div id="goto" class="popup-overlay" style="display: none">
+      <div class="gotopopup">
+        <div class="my-overview">
+          <a href="#act" style="color:black">My OverView</a>
+        </div>
+        <div class="my_workout_plan">
+          <a href="#plan" style="color:black">My WorkOut Plan</a>
+        </div>
+        <div class="my-gym_sapce">
+          <a href="#gym" style="color:black">The Gym Space</a>
+        </div>
+      </div>
+    </div>
+ 
+
+    <script>
+
+      var loginButton = document.getElementById("go");
+      if (loginButton) {
+        loginButton.addEventListener("click", function() {
+          var popup = document.getElementById("goto");
+          if (!popup) return;
+          var popupStyle = popup.style;
+          if (popupStyle) {
+            popupStyle.display = "flex";
+            popupStyle.zIndex = 100;
+            popupStyle.backgroundColor = "rgba(113, 113, 113, 0.3)";
+            popupStyle.alignItems = "center";
+            popupStyle.justifyContent = "center";
+          }
+          popup.setAttribute("closable", "");
+
+          var onClick =
+            popup.onClick ||
+            function(e) {
+              if (e.target === popup && popup.hasAttribute("closable")) {
+                popupStyle.display = "none";
+              }
+            };
+          popup.addEventListener("click", onClick);
+        });
+      }
+    </script>
 </body>
 
 </html>
