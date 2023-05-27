344f6ad568e5b21d8166ca5da039b57f9ec2267b
AbdouGitt <bassel.grine@ensia.edu.dz>
2023-05-27 22:00:47 +0100

    Tweaks to homepage before login

---
+++ footer.php
@@ -0,0 +1,33 @@
+<head>
+    <link rel="stylesheet" href="footer.css">
+    <script src="footer.js"></script>
+    <title>footer</title>
+</head>
+<body>
+<div class="logo1">
+
+</div>
+<footer>
+    <a class="social-link1">Contact us</a>
+
+    <a href="#" title="Instagram" class="social-link">
+        <img src="public/instagram.png" alt="Instagram">Instagram
+    </a>
+    <a href="#" title="Facebook" class="social-link">
+        <img src="public/facebook.png" alt="Facebook">Facebook
+    </a>
+    <a href="#" title="LinkedIn" class="social-link">
+        <img src="public/linkedin.png" alt="LinkedIn"> LinkedIn
+    </a>
+
+    <a href="mailto:4ARMS@gmail.com" title="Email" class="social-link">
+        <img src="public/gmail.png" alt="Email">Email
+    </a>
+
+    <a href="tel:+213557238309" title="Phone" class="social-link">
+        <img src="public/phone-call.png" alt="Phone">Phone
+    </a>
+    <div class="quote"><img class="frame-icon6" alt="" src="./public/frame6.svg"/>
+        <div class="arms"></div>Get Fit OR Die Trying</div>
+</footer>
+</body>
